ccdd4283c9b37b259cd853d5bbea6032701631c5
KIMJISUNG0 <131224231+KIMJISUNG0@users.noreply.github.com>
2025-09-26 09:18:54 -0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,53 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('photo-bridge-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/socket.io/socket.io.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+--- a/public/service-worker.js
++++ b/public/service-worker.js
+@@
+-self.addEventListener('install', e => {
+-  e.waitUntil(
+-    caches.open('photo-bridge-cache').then(cache => {
+-      return cache.addAll([
+-        '/',
+-        '/index.html',
+-        '/socket.io/socket.io.js'
+-      ]);
+-    })
+-  );
+-});
++const CACHE = 'photo-bridge-cache-v2';
++
++self.addEventListener('install', (e) => {
++  e.waitUntil(
++    caches.open(CACHE).then((cache) => {
++      return cache.addAll([
++        '/',
++        '/index.html',
++        '/manifest.json'
++      ]);
++    })
++  );
++});
++
++self.addEventListener('activate', (e) => {
++  e.waitUntil(
++    caches.keys().then(keys =>
++      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
++    )
++  );
++});
+ 
+-self.addEventListener('fetch', e => {
+-  e.respondWith(
+-    caches.match(e.request).then(response => {
+-      return response || fetch(e.request);
+-    })
+-  );
+-});
++self.addEventListener('fetch', (e) => {
++  const url = new URL(e.request.url);
++  // ❗ Socket.IO 및 CDN 은 캐시 개입 금지 (네트워크 우선)
++  if (url.pathname.startsWith('/socket.io/') || url.hostname === 'cdn.socket.io') {
++    return; // 기본 네트워크 처리
++  }
++  e.respondWith(
++    caches.match(e.request).then((resp) => resp || fetch(e.request))
++  );
++});
